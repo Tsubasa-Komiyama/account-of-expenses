40fe01a041363e7fa0499c1e8474d9f505e962f7
Tsubasa-Komiyama <63089849+Tsubasa-Komiyama@users.noreply.github.com>
2023-08-16 11:58:33 +0900

    firebaseログインを再作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import { getFirebaseApp } from "@/firebase/firebse";
 import styles from "./page.module.css";
-import { getApp } from "firebase/app";
-
-getFirebaseApp();
 
 export default function Home() {
-  console.log(getApp());
-  console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-  console.log("Hello world!");
 
   return (
     <main className={styles.main}>
-      console.log("Hello world!");
       <h1>Hello world!</h1>
     </main>
   );
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,24 +1,25 @@
 import {
-  FirebaseApp,
   getApps,
   initializeApp,
-  getApp,
   FirebaseOptions,
 } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-export const config: FirebaseOptions = {
+export const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_AMESUREMENT_ID,
-  databaseURL: "https://account-of-expenses-default-rtdb.firebaseio.com/",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MESUREMENT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE,
 };
 
-const app = initializeApp(firebaseConfig);
+if (!getApps()?.length) {
+  initializeApp(firebaseConfig);
+}
 
-export const getFirebaseApp = (): FirebaseApp => {
-  return !getApps().length ? initializeApp(config) : getApp();
-};
+export const auth = getAuth();
+export const db = getFirestore();
